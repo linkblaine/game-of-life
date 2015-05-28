ea6ae7edab0346e7c3e6921cafc5f022f6f91692
Zack Mance <zmance@gmail.com>
2015-05-28 11:19:25 -0400

    Adds cell neighborKeys and specs

--- spec/javascripts/cellSpec.js
+++ spec/javascripts/cellSpec.js
@@ -23,8 +23,8 @@ describe('Cell', function(){
     });
   });
 
-  describe('Key Generation', function(){
-    it('Should have a key function', function(){
+  describe('getKey', function(){
+    it('Should have a getKey function', function(){
       expect(testCell.getKey).toBeDefined();  
     });
 
@@ -33,7 +33,7 @@ describe('Cell', function(){
     });
   });
   
-  describe('Cell isAlive', function(){
+  describe('isAlive', function(){
 
     it('Should initalize /w false', function(){
       expect(testCell.isAlive).toBeDefined();  
@@ -43,6 +43,61 @@ describe('Cell', function(){
     it('Should able to initalize as true', function(){
       var aliveCell = new Cell(0, 0, true );
       expect( aliveCell.isAlive ).toBe( true ); 
+    });
+  });
+
+  describe('neighborKeys', function(){
+    beforeEach(function(){
+      testCell = new Cell(1,1); 
+    });
+    
+    it('Should have a nighborKeys function', function(){
+      expect(testCell.neighborKeys()).toBeDefined(); 
+    });
+
+    it('Should know the top-left neighbor', function(){
+      testKey = makeKey(testCell.x, testCell.y, -1, 1);
+      expect(testCell.neighborKeys()).toContain( testKey );
+    });
+
+    it('Should know the top-middle neighbor', function(){
+      testKey = makeKey(testCell.x, testCell.y, -1, 1);
+      expect(testCell.neighborKeys()).toContain( testKey );
+    });
+
+    it('Should know the top-right neighbor', function(){
+      testKey = makeKey(testCell.x, testCell.y, -1, 1);
+      expect(testCell.neighborKeys()).toContain( testKey );
+    });
+    
+    it('Should know the middle-left neighbor', function(){
+      testKey = makeKey(testCell.x, testCell.y, -1, 1);
+      expect(testCell.neighborKeys()).toContain( testKey );
+    });
+
+    it('Should know the middle-right neighbor', function(){
+      testKey = makeKey(testCell.x, testCell.y, -1, 1);
+      expect(testCell.neighborKeys()).toContain( testKey );
+    });
+
+    it('Should know the bottom-left neighbor', function(){
+      testKey = makeKey(testCell.x, testCell.y, -1, 1);
+      expect(testCell.neighborKeys()).toContain( testKey );
+    });
+
+    it('Should know the bottom-middle neighbor', function(){
+      testKey = makeKey(testCell.x, testCell.y, -1, 1);
+      expect(testCell.neighborKeys()).toContain( testKey );
+    });
+
+    it('Should know the bottom-right neighbor', function(){
+      testKey = makeKey(testCell.x, testCell.y, -1, 1);
+      expect(testCell.neighborKeys()).toContain( testKey );
+    });
+
+    it('Should not contain itself', function(){
+      testKey = makeKey(testCell.x, testCell.y, 0, 0);
+      expect(testCell.neighborKeys()).not.toContain( testKey );
     });
   });
 });
--- src/cell.js
+++ src/cell.js
@@ -8,3 +8,20 @@ var Cell = function(x, y, isAlive){
 Cell.prototype.getKey = function(){
   return this.x + ":" + this.y;
 };
+
+Cell.prototype.neighborKeys = function(){
+  var keys = [];
+  for(var i = -1; i <= 1; i++){
+    for(var j = -1; j <= 1; j++){
+      key = makeKey(this.x, this.y, i, j);
+      if( this.getKey() != key){
+        keys.push( key );
+      }
+    } 
+  }
+  return keys;
+};
+
+var makeKey = function(x, y, xOffset, yOffset){
+  return (x + xOffset) + ":" + (y+yOffset); 
+};
