682375c5d736729978466d7adbb8a5a424af6714
Zack Mance <zmance@gmail.com>
2015-06-01 22:52:23 -0400

    full draw

--- src/main.js
+++ src/main.js
@@ -2,16 +2,32 @@ $(document).ready(function(){
 
   var game = new Game();
   drawBoard( game.numberOfRows, game.numberOfColumns);
-  for( var i = 0; i < game.numberOfIterations; i++){
+  paintCells( game.board );
+
+  window.setInterval(function(){
     game.iterate();
-  }
+    paintCells( game.board );
+  }.bind(document), 500);
+
 });
+
+var paintCells= function(board){
+  
+  $.each(board.coordinates, function(cell){
+    point = cell.split(':');
+   
+    var rowId = 'row-' + point[1]; 
+    var colId = 'column-' + point[0]; 
+    $('#'+rowId + '>#' +colId).toggleClass('alive', board.coordinates[cell].isAlive  ); 
+  });  
+
+}
 
 var drawBoard = function( rows, columns){
   
   for(var i = 0; i < rows; i++){
     var rowId = 'row-' + i;
-      $('#game-board').append("<tr id="+ rowId +" class='row'></tr>");
+    $('#game-board').append("<tr id="+ rowId +" class='row'></tr>");
 
     for(var j = 0; j < columns; j++){
       var columnId = 'column-' + j;
