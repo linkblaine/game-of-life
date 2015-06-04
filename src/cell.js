d9e2ca200a5469e55d5e4276c057b87f19e56597
Zack Mance <zmance@gmail.com>
2015-06-04 17:32:21 -0400

    Fixes up the key, and neighbor methods

--- src/cell.js
+++ src/cell.js
@@ -2,25 +2,23 @@ var Cell = function(x, y, isAlive){
   this.x = x;
   this.y = y;
   this.isAlive = isAlive || false;
-  this.coordinates = {"x": x, "y": y};
   this.board;
-};
-
-Cell.prototype.getKey = function(){
-  return this.x + ":" + this.y;
+  this.key = this.x + ":" + this.y;
 };
 
 Cell.prototype.neighborKeys = function(){
   var keys = [];
+
   for(var i = -1; i <= 1; i++){
     for(var j = -1; j <= 1; j++){
       key = makeKey(this.x, this.y, i, j);
-      if( this.getKey() != key){
-        keys.push( key );
-      }
+      keys.push( key );
     } 
   }
+
+  keys.splice( keys.indexOf( this.key ), 1 );
   return keys;
+
 };
 
 var makeKey = function(x, y, xOffset, yOffset){
