0157ce4eeb2768f7120fe739050f35ec14c064e7
Zack Mance <zmance@gmail.com>
2015-06-04 18:44:35 -0400

    pulls out drawing code into a view class

--- src/main.js
+++ src/main.js
@@ -21,31 +21,14 @@ $(document).ready(function(){
   '-----xx-xx---x------,' ;
 
   var game = new Game(initialState);
+  var view = new View(game);
 
-  drawBoard( game.rowCount, game.columnCount);
-  paintCells( game.cells );
+  view.drawBoard();
 
   setInterval( function(){
     game.iterate();
-    paintCells( game.cells );
+    view.paintCells();
   }, game.timeout);
 
 });
 
-var paintCells= function(cells){
-  cells.forEach( function(cell){
-    $( '#row-' + cell.x + '>' +'#column-' + cell.y ).toggleClass( 'alive', cell.isAlive  ); 
-  });  
-}
-
-var drawBoard = function( rows, columns){
-  for(var i = 0; i < rows; i++){
-    var rowId = 'row-' + i;
-    $('#game-board').append("<tr id="+ rowId +" class='row'></tr>");
-    for(var j = 0; j < columns; j++){
-      var columnId = 'column-' + j;
-      $('#'+rowId).append("<td id=" +columnId +" class='column'></td>");
-    }
-  }
-  
-}
---
+++ src/view.js
@@ -0,0 +1,21 @@
+var View = function( game ){
+  this.game = game;
+  this.paintCells();
+};
+
+View.prototype.paintCells = function(){
+  this.game.cells.forEach( function(cell){
+    $( '#row-' + cell.x + '>' +'#column-' + cell.y ).toggleClass( 'alive', cell.isAlive  ); 
+  });  
+}
+
+View.prototype.drawBoard = function(){
+  for(var i = 0; i < this.game.rowCount; i++){
+    var rowId = 'row-' + i;
+    $('#game-board').append("<tr id="+ rowId +" class='row'></tr>");
+    for(var j = 0; j < this.game.columnCount; j++){
+      var columnId = 'column-' + j;
+      $('#'+rowId).append("<td id=" +columnId +" class='column'></td>");
+    }
+  }
+}
