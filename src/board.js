1562ca05aa407506b8f87db665c374d10654a8cc
Zack Mance <zmance@gmail.com>
2015-06-04 18:37:42 -0400

    rename variable

--- src/board.js
+++ src/board.js
@@ -1,25 +1,23 @@
 var Board = function(cells){
   this.coordinates = {}; 
-
   for(var i = 0; i < cells.length; i++){
     cell = cells[i];
     cell.board = this;
     this.coordinates[cell.key] = cell;
   }
-
 };
 
 Board.prototype.getCellStatus = function( cell ){
   var neighbors = cell.neighborKeys();
-  var neighborCount = 0;
+  var livingNeighbors = 0;
+
   for(var i = 0; i < neighbors.length; i++){
     neighborCell = this.coordinates[ neighbors[i] ];
     if(neighborCell && neighborCell.isAlive){
-      neighborCount++;
+      livingNeighbors++;
     }
   }
-
-  return this.gameLogic( cell.isAlive, neighborCount);
+  return this.gameLogic(cell.isAlive,livingNeighbors);
 };
 
 Board.prototype.gameLogic = function( isAlive, neighborCount ){
