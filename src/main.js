d0c67fac3acc189c96b4697a830b90482d644ea6
Zack Mance <zmance@gmail.com>
2015-06-04 17:45:50 -0400

    modifies how the board is drawn

--- src/main.js
+++ src/main.js
@@ -23,33 +23,25 @@ $(document).ready(function(){
   var game = new Game(initialState);
 
   drawBoard( game.rowCount, game.columnCount);
-  paintCells( game.board );
+  paintCells( game.cells );
 
   setInterval( function(){
     game.iterate();
-    paintCells( game.board );
+    paintCells( game.cells );
   }, game.timeout);
 
 });
 
-var paintCells= function(board){
-  
-  $.each(board.coordinates, function(cell){
-    point = cell.split(':');
-
-    var rowId = 'row-' + point[0]; 
-    var colId = 'column-' + point[1]; 
-    $('#'+rowId + '>#' +colId).toggleClass('alive', board.coordinates[cell].isAlive  ); 
+var paintCells= function(cells){
+  cells.forEach( function(cell){
+    $( '#row-' + cell.x + '>' +'#column-' + cell.y ).toggleClass( 'alive', cell.isAlive  ); 
   });  
-
 }
 
 var drawBoard = function( rows, columns){
-  
   for(var i = 0; i < rows; i++){
     var rowId = 'row-' + i;
     $('#game-board').append("<tr id="+ rowId +" class='row'></tr>");
-
     for(var j = 0; j < columns; j++){
       var columnId = 'column-' + j;
       $('#'+rowId).append("<td id=" +columnId +" class='column'></td>");
