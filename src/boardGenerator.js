15e8e17fc03594373d169d72595f71f40709c9b2
Zack Mance <zmance@gmail.com>
2015-06-06 11:12:01 -0400

    Adds Random Board Generator to view.

---
+++ src/boardGenerator.js
@@ -0,0 +1,16 @@
+var BoardGenerator = function(){
+
+  var generate = function( rows, columns ){
+    var board= '';
+
+    for(var i=0; i<rows; i++){
+      for(var j=0; j<rows; j++){
+        board += ((( Math.floor( Math.random() * 10 ) + 1) % 2) ? 'x' : '-' );
+      } 
+      board += ',';
+    }  
+    return board; 
+  };
+
+  return {generate: generate}
+}();
--- src/main.js
+++ src/main.js
@@ -1,28 +1,11 @@
 $(document).ready(function(){
 
-  var init = 
-  '--------------------,' +
-  '---xxxx-----xx--x---,' +
-  '------xx---xx--x----,' +
-  '----xx---xxx--x-x---,' +
-  '-----xx-xx---x------,' +
-  'x-x---xx-----x------,' +
-  'x-x----xx----x-xx---,' +
-  'x--xx----------xxx--,' +
-  'xxx---xx--xxx--x----,' +
-  'x---xxx-----xx--xx--,' +
-  'x--------xxx--------,' +
-  '-----xx-xx---x------,' +
-  '--x--xx-xx---x--xx--,' +
-  '--x--x-----x-xxx---x,' +
-  '--xx-xxxxx---xxx----,' +
-  '--x--xx-xx---xxx----,' +
-  '--x--xx-xx---x------,' +
-  '---xxxx-xx--xx------,' ;
-
+  var init = BoardGenerator.generate( 65, 65 );
   var params = {initialState: init, timeout: 100}
+
   var game = new Game(params);
   var view = new View(game);
+
   var changedCellsCache = [];
 
   var interval = setInterval( function(){
