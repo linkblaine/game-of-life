956e580f0ade9d6bb504ba437ed0bc104966c6da
Zack Mance <zmance@gmail.com>
2015-06-11 15:48:28 -0400

    Removes row col limit

--- src/boardGenerator.js
+++ src/boardGenerator.js
@@ -2,15 +2,13 @@ var BoardGenerator = function(){
 
   var generate = function( rows, columns ){
     var board= '';
-
-    for(var i=0; i<rows; i++){
-      for(var j=0; j<rows; j++){
+    for(var i=0; i < rows; i++){
+      for(var j=0; j < columns; j++){
         board += ((( Math.floor( Math.random() * 10 ) + 1) % 2) ? 'x' : '-' );
       } 
       board += ',';
     }  
     return board; 
   };
-
   return {generate: generate}
 }();
